c58450448f0eef4d633265b939f6e3e95ab5baac
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-07-23 13:42:52 +0300

    f

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Переменные
+
+const age = 10;
+const totalPrice = 200.2;
+const userName = "Chelsy";
+const message = "Welcome";
+// input
+
 // const shouldRenew = confirm("Хотите продлить подписку?");
 // console.log(shouldRenew);
 // let quantity = prompt("Введите количество товара");
