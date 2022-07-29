b8d7cc3063a9cc0846c1be62104568580efc935a
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-07-29 11:57:31 +0300

    11

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Переменные
 
-const age = 10;
-const totalPrice = 200.2;
-const userName = "Chelsy";
-const message = "Welcome";
+// const age = 10;
+// const totalPrice = 200.2;
+// const userName = "Chelsy";
+// const message = "Welcome";
+// const isOpen = "True";
+// const shouldConfirm = "False";
+// console.log("Total", totalPrice);
+// const type = typeof isOpen;
+
+// console.log(type);
+
 // input
 
 // const shouldRenew = confirm("Хотите продлить подписку?");
@@ -12,3 +19,44 @@ const message = "Welcome";
 // quantity = Number(quantity);
 // console.log(quantity);
 // console.log(typeof quantity);
+
+// number
+
+// let element = "50.23px";
+// console.log(Number.parseInt(element));
+// let elementHeight = "207.56px";
+// elementHeight = Number.parseFloat(elementHeight);
+// console.log("Height -", elementHeight);
+// let solar = "1073.5695485px";
+// solar = Number.parseFloat(solar);
+// console.log(solar.toFixed(3));
+// function checkAge(age) {
+// 	if (age >= 18) {
+// 		return "You are an adult";
+// 	}
+
+// 	return "You are a minor";
+// 	console.log();
+// }
+// checkAge(18);
+
+// function checkForSpam(message) {
+// 	let result;
+
+// 	if (message.toLowerCase().includes("spam")) {
+// 		result = true;
+// 	} else if (message.toLowerCase().includes("sale")) {
+// 		result = true;
+// 	} else {
+// 		result = false;
+// 	}
+
+// 	console.log(result);
+// }
+// checkForSpam("Latest technology news");
+
+// function simple(num, order) {
+// 	let total = num * order;
+// 	console.log(total);
+// }
+// simple(3, 5);
