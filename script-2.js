bf60a642aa8a8293199ab874d576e9063362f7ff
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-07-31 11:02:10 +0300

    -

--- script-2.js
+++ script-2.js
@@ -1,7 +1,7 @@
 // // Switch
 
-const stars = 3;
-let price;
+// const stars = 3;
+// let price;
 
 // if (stars === 1 || stars === 2) {
 // 	price = 20;
@@ -13,21 +13,39 @@ let price;
 // 	console.log("Такого количества звезд нет");
 // }
 
-switch (stars) {
-	case 1:
-	case 2:
-		price = 20;
-		break;
+// switch (stars) {
+// 	case 1:
+// 	case 2:
+// 		price = 20;
+// 		break;
 
-	case 3:
-	case 4:
-		price = 50;
-		break;
-	case 5:
-		price = 100;
-		break;
-	default:
-		console.log("Такого количества звезд нет");
-}
+// 	case 3:
+// 	case 4:
+// 		price = 50;
+// 		break;
+// 	case 5:
+// 		price = 100;
+// 		break;
+// 	default:
+// 		console.log("Такого количества звезд нет");
+// }
 
-console.log(price);
+// console.log(price);
+
+// const option = 4;
+// let message;
+
+// switch (option) {
+// 	case 1:
+// 		message = "Вы можете забрать посылку завтра с 12-00";
+// 		break;
+// 	case 2:
+// 		message = "Курьер привезет вам посылку завтра после 19:00";
+// 		break;
+// 	case 3:
+// 		message = "Почта будет отправлена в течении 2х дней";
+// 		break;
+// 	default:
+// 		message = "Менеджер вам перезвонит";
+// }
+// console.log(message);
