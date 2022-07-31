8f321490e010a9eea58309cebb94f1706a63ae18
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-07-31 13:06:47 +0300

    ==

--- script-2.js
+++ script-2.js
@@ -49,3 +49,46 @@
 // 		message = "Менеджер вам перезвонит";
 // }
 // console.log(message);
+
+// Цикл for
+// for(инициализация начальное значение; условие до какого выполняется цикл; на сколько изменяется)
+// {тело цикла }
+
+// for (let i = 9; i <= 100; i += 5) {
+// 	console.log("Число", i);
+// }
+
+// const minSalary = 500;
+// const maxSalary = 5000;
+// const employers = 4;
+// let totalSalary = 0;
+// for (let i = 1; i <= 5; i += 1) {
+// 	const salary = Math.round(Math.random() * (maxSalary - minSalary) + minSalary);
+// 	console.log(`Total salary employ ${i} - ${salary}`);
+// 	totalSalary += salary;
+// }
+// console.log("Total", totalSalary);
+
+const min = 0;
+const max = 10;
+let total = 0;
+
+// for (let i = min; i <= max; i += 1) {
+// 	if (i % 2 === 0) {
+// 		console.log("None:", i);
+// 		total += i;
+// 	} else {
+// 		console.log("Odd:", i);
+// 	}
+// }
+// console.log("total", total);
+
+for (let i = min; i <= max; i += 1) {
+	if (i % 2 !== 0) {
+		console.log("Odd:", i);
+		continue;
+	}
+	console.log("None:", i);
+	total += i;
+}
+console.log("total", total);
